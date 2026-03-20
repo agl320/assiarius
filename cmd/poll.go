cea6bc0c34e6fc72aadc0fd4e3cf2ff32e4083f7
Andrew Lai <agl13@sfu.ca>
2026-03-20 02:15:25 -0700

    Update poll.go

--- cmd/poll.go
+++ cmd/poll.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"assiarius/internal/poll"
+	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -9,19 +11,30 @@ import (
 
 func pollCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "poll [screener] [interval]",
+		Use:   "poll [screener] [intervalSeconds]",
 		Short: "Poll a Finviz screener",
 		Args:  cobra.RangeArgs(1, 2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := cmd.Context()
 
+			// default
 			interval := 15 * time.Second
 			if len(args) == 2 {
-				parsed, err := time.ParseDuration(args[1])
-				if err != nil {
-					return err
+				if seconds, err := strconv.Atoi(args[1]); err == nil {
+					if seconds <= 0 {
+						return fmt.Errorf("intervalSeconds must be a positive integer, got %d", seconds)
+					}
+					interval = time.Duration(seconds) * time.Second
+				} else {
+					parsed, err := time.ParseDuration(args[1])
+					if err != nil {
+						return fmt.Errorf("invalid interval %q: use an integer number of seconds (e.g. 10) or a duration (e.g. 10s, 1m): %w", args[1], err)
+					}
+					if parsed <= 0 {
+						return fmt.Errorf("interval must be > 0, got %s", parsed)
+					}
+					interval = parsed
 				}
-				interval = parsed
 			}
 
 			return poll.StartPoller(ctx, args[0], interval)
